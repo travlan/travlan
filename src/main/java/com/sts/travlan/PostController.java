ef4e6597e1af95d0805015df1713233a656ea663
DeCrazy <hw12011@gmail.com>
2019-11-18 15:25:50 +0900

    minor changes

--- src/main/java/com/sts/travlan/PostController.java
+++ src/main/java/com/sts/travlan/PostController.java
@@ -82,7 +82,7 @@ public class PostController {
 			// img 태그의 title 속성을 원본파일명으로 적용시켜주기 위함
 
 			sFileInfo += "&sFileName=" + filename;
-			sFileInfo += "&sFileURL=" + "/resource/photo_upload/" + realFileNm;
+			sFileInfo += "&sFileURL=" + "Travlan/resource/photo_upload/" + realFileNm;
 			PrintWriter print = response.getWriter();
 
 			print.print(sFileInfo);
